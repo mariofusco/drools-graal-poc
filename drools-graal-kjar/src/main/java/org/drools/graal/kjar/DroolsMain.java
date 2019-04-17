c89a5240f69373701099e5d354b242782241c32c
mariofusco <mario.fusco@gmail.com>
2019-04-17 15:41:38 +0200

    update example

--- drools-graal-kjar/src/main/java/org/drools/graal/kjar/DroolsMain.java
+++ drools-graal-kjar/src/main/java/org/drools/graal/kjar/DroolsMain.java
@@ -5,7 +5,6 @@ import org.drools.simple.project.Result;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
-import org.kie.api.runtime.rule.FactHandle;
 
 public class DroolsMain {
 
@@ -13,37 +12,12 @@ public class DroolsMain {
         KieContainer kcontainer = KieServices.get().newKieClasspathContainer();
         KieSession ksession = kcontainer.newKieSession();
 
-        Person mark = new Person("Mark", 37);
-        Person edson = new Person("Edson", 35);
-        Person mario = new Person("Mario", 40);
-
         Result result = new Result();
         ksession.insert( result );
-        FactHandle markFH = ksession.insert(mark);
-        FactHandle edsonFH = ksession.insert(edson);
-        FactHandle marioFH = ksession.insert(mario);
+        ksession.insert(new Person(args[0], Integer.parseInt( args[1] )));
 
         ksession.fireAllRules();
-        if (!"Mario is older than Mark".equals( result.getValue() )) {
-            throw new IllegalStateException();
-        }
 
-        result.setValue( null );
-        ksession.delete( marioFH );
-        ksession.fireAllRules();
-        if (result.getValue() != null) {
-            throw new IllegalStateException();
-        }
-
-        mark.setAge( 34 );
-//        ksession.update( markFH, mark, "age" );
-        ksession.update( markFH, mark );
-
-        ksession.fireAllRules();
-        if (!"Edson is older than Mark".equals( result.getValue() )) {
-            throw new IllegalStateException();
-        }
-
-        System.out.println("SUCCESS!");
+        System.out.println(result);
     }
 }
